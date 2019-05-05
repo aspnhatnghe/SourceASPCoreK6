982699c1bf9a50574bf6d629d36c71dbf232ae6a
NN <NN@PC15>
2019-05-05 08:22:50 +0700

    Source day 1: Intro, review CSharp

---
+++ D01_Intro/D01_Intro/Controllers/DemoController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D01_Intro.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DemoController : Controller
+    {
+        public IActionResult OOP()
+        {
+            //tạo đối tượng
+            HinhChuNhat hcn = new HinhChuNhat()
+            {
+                Dai = 4,
+                Rong = 2.2
+            };
+
+            return Json(hcn);
+        }
+
+        public IActionResult Mang()
+        {
+            return View();
+        }
+
+        public IActionResult XuLyHinhTron(int? BanKinh)
+        {
+            //dấu chấm hỏi ở parameter cho phép null (trừ kiểu string)
+            if (BanKinh.HasValue)
+            {
+                //Dùng ViewBag để gửi giá trị từ Action qua View
+                ViewBag.ChuVi = Math.Round(2 * BanKinh.Value * Math.PI, 2);
+                ViewBag.DienTich = Math.Pow(BanKinh.Value, 2) * Math.PI;
+                ViewBag.BanKinh = BanKinh;
+            }
+
+            return View();
+        }
+
+        public IActionResult MyForm()
+        {
+            return View();
+        }
+
+        public int SoNguyen()
+        {
+            return 2;
+        }
+
+        public int Tong(int a, int b)
+        {
+            return a + b;
+        }
+
+        //Ghi chú 1 dòng
+        /*
+         * Nhiều dòng
+         */
+        double luongCoBan = 1490;
+
+        /// <summary>
+        /// Hàm xuất hiện câu chào
+        /// </summary>
+        /// <param name="hoTen">tên</param>
+        /// <returns>câu chào</returns>
+        public string Chao(string hoTen = "")
+        {
+            return $"Xin chào {hoTen}";
+        }
+    }
+}
---
+++ D01_Intro/D01_Intro/Models/HinhChuNhat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D01_Intro.Models
+{
+    public class HinhChuNhat
+    {
+        //Field
+        private double dai;
+        //sinh ra Property
+        public double Dai
+        {
+            get { return dai; }
+            set
+            {
+                if (value > 0)
+                    dai = value;
+                else throw new Exception("Dai am");
+            }
+        }
+
+        //Automatic Property
+        public double Rong { get; set; }
+
+        //Property chỉ get (từ C# 7.0 trở lên)
+        public double ChuVi => (Dai + Rong) * 2;//C# 7.0
+
+        public double S => Dai * Rong;//C# 7.0
+        public double DienTich
+        {
+            get { return Dai * Rong; }
+        }
+
+        public string Chuoi()
+        {
+            return $"HCN dài {Dai}, rộng {Rong} có diện tích {DienTich}, chu vi {ChuVi}";
+        }
+    }
+}
