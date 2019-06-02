1ac1f3ff4ec21962510d4a2b251ad9a18b6591e1
NN <NN@PC15>
2019-06-02 11:55:20 +0700

    D08 - Validation

---
+++ D08_Validation/D08_Validation/Models/BirthDateCheckAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace D08_Validation.Models
+{
+    public class BirthDateCheckAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime data = (DateTime) value;
+            return data < DateTime.Now;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime data = (DateTime)value;
+            if(data < DateTime.Now)
+            {
+                return ValidationResult.Success;
+            }
+            return new ValidationResult("Không thể sinh trong tương lai");
+        }        
+    }
+}
