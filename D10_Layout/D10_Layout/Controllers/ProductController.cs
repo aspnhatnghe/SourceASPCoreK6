e6f43cf2e9c0fe15f9e6948ed5a808b076d3c153
NN <NN@PC15>
2019-06-09 11:44:51 +0700

    Design layout template

---
+++ D10_Layout/D10_Layout/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace D10_Layout.Controllers
+{
+    public class ProductController : Controller
+    {
+        //default: Grid
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
