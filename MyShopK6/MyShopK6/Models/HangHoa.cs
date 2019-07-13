6456126e843026c5a2eaaf8764b3dc49dc14f189
Hien Luong <hyhien@gmail.com>
2019-07-13 17:39:17 +0700

    Update layout

---
+++ MyShopK6/MyShopK6/Models/HangHoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyShopK6.Models
+{
+    [Table("HangHoa")]
+    public class HangHoa
+    {
+        [Key]
+        public int MaHh { get; set; }
+        [MaxLength(50)]
+        public string TenHh { get; set; }
+        [MaxLength(150)]
+        public string Hinh { get; set; }
+        public string MoTa { get; set; }
+        public double DonGia { get; set; }
+        public int SoLuong { get; set; }
+        public int MaLoai { get; set; }
+        [ForeignKey("MaLoai")]
+        public Loai Loai { get; set; }
+    }
+}
--- MyShopK6/MyShopK6/Startup.cs
+++ MyShopK6/MyShopK6/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MyShopK6.Models;
 
 namespace MyShopK6
 {
@@ -33,6 +35,8 @@ namespace MyShopK6
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<MyDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("MyShopK6")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,8 +60,14 @@ namespace MyShopK6
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                  );
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
             });
         }
     }
